01929d830d96ddf62c4e8cb4881aef7ca351475e
Berkay Yelken <CelalBerkay.Yelken@vodafone.com>
2023-09-22 17:53:03 +0300

    ms init

---
+++ src/main/java/io/github/berkayelken/team/document/TeamDocumentApplication.java
@@ -0,0 +1,13 @@
+package io.github.berkayelken.team.document;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamDocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamDocumentApplication.class, args);
+	}
+
+}
