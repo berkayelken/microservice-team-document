adbd2b221b7045054346721a8167868627eafd67
Berkay Yelken <CelalBerkay.Yelken@vodafone.com>
2023-09-25 22:40:05 +0300

    expectation type enum is added

---
+++ src/main/java/io/github/berkayelken/team/document/domain/review/ExpectationType.java
@@ -0,0 +1,5 @@
+package io.github.berkayelken.team.document.domain.review;
+
+public enum ExpectationType {
+	TECHNICAL, SOFT, NECESSITY, TO_CUSTOMER, TO_COMPANY, TO_ORGANIZATION, FROM_CUSTOMER, FROM_COMPANY, FROM_ORGANIZATION
+}
